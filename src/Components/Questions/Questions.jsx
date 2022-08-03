08b29cb095a68fe16e48b502749d3ffa24e2f405
unknown <maheshasiram@gmail.com>
2022-08-03 19:40:30 +0530

    questions

--- src/Components/Questions/Questions.jsx
+++ src/Components/Questions/Questions.jsx
@@ -4,7 +4,7 @@ function Questions(props) {
     return (
         <div className='questionsComponent'>
             <h2 className='text-left'>Questions</h2>
-            
+            <div className='unAssignedMsg'>No Technology is assigned to create question</div>
         </div>
     );
 }
